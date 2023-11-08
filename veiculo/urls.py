d962ac775292c294cfa0fe5a2396f6661a0b7893
Paulo Júnior <paulojunior2000@gmail.com>
2023-11-08 20:41:15 -0300

    Aula 08/11

--- veiculo/urls.py
+++ veiculo/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from veiculo.views import FotoVeiculo, ListarVeiculos
+from veiculo.views import FotoVeiculo, ListarVeiculos, APIListarCriarVeiculos, APIObterEditarDeletarVeiculos
 
 urlpatterns = [
     path('', ListarVeiculos.as_view(), name='veiculos'),
+    path('api/', APIListarCriarVeiculos.as_view(), name='api-listar-criar-veiculos'),
+    path('api/<int:pk>/', APIObterEditarDeletarVeiculos.as_view(), name='api-obter-editar-deletar-veiculos'),
     path('fotos/<str:arquivo>/', FotoVeiculo.as_view(), name='foto-veiculo')
 ]
--- veiculo/views.py
+++ veiculo/views.py
@@ -1,10 +1,14 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import FileResponse, Http404
 from django.views.generic import ListView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from sistema2.bibliotecas import LoginObrigatorio
 from veiculo.models import Veiculo
 
+from veiculo.serializers import SerializadorVeiculo
 
 class ListarVeiculos(LoginObrigatorio, ListView):
     """
@@ -27,3 +31,26 @@ class FotoVeiculo(LoginObrigatorio):
             raise Http404("Foto não existe ou acesso não altorizado")
         except Exception as exception:
             raise Exception
+
+class APIListarCriarVeiculos(ListCreateAPIView):
+    """
+    View para listar e criar instâncias de veículos (por meio da API REST)
+    """
+    serializer_class = SerializadorVeiculo
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Veiculo.objects.all()
+
+
+class APIObterEditarDeletarVeiculos(RetrieveUpdateDestroyAPIView):
+    """
+    View para obter, editar e deletar instâncias de veículos (por meio da API REST)
+    """
+    serializer_class = SerializadorVeiculo
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Veiculo.objects.all()
